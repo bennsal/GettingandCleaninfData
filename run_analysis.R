9a81ee44bafe9f1957b13d6d3ab6d698dacbccf6
Ben Amos <ben@teule.org.uk>
2014-12-17 23:22:06 +0000

    Initial Commit

---
+++ run_analysis.R
@@ -0,0 +1,36 @@
+# Setup
+setwd("~/R")
+library(dplyr)
+# Read Data
+train <- read.table("UCI HAR Dataset/train/X_train.txt", sep="", header=FALSE )
+train[,562] <- read.table("UCI HAR Dataset/train/y_train.txt", sep="", header=FALSE )
+train[,563] <- read.table("UCI HAR Dataset/train/subject_train.txt", sep="", header=FALSE )
+test <- read.table("UCI HAR Dataset/test/X_test.txt", sep="", header=FALSE )
+test[,562] <- read.table("UCI HAR Dataset/test/y_test.txt", sep="", header=FALSE )
+test[,563] <- read.table("UCI HAR Dataset/test/subject_test.txt", sep="", header=FALSE )
+labels <- read.table("UCI HAR Dataset/activity_labels.txt", sep="", header=FALSE )
+headings <- read.table("UCI HAR Dataset/features.txt", sep="", header=FALSE )
+
+# Merge test and training data tables
+merged = rbind(train, test)
+# Correct heading labels
+headings[,2] = gsub('-mean', 'Mean', headings[,2])
+headings[,2] = gsub('-std', 'Std', headings[,2])
+headings[,2] = gsub('[-()]', '', headings[,2])
+# Select columns for mean and sd
+columns_needed <- grep(".*Mean.*|.*Std.*", headings[,2])
+# Remove columns that are not wanted
+headings <- headings[columns_needed,]
+# Add subject and activity columns
+columns_needed <- c(columns_needed, 562, 563)
+# remove unwanted columns in data field
+merged <- merged[,columns_needed]
+# add column names
+colnames(merged) <- c(headings$V2, "activity", "aubject")
+colnames(merged) <- tolower(colnames(merged))
+# add activity labels
+merged[,87] <- labels[merged[,87],2]
+
+final_data <- ddply(merged, .(subject, sctivity), function(x) colMeans(x[,1:86]))
+write.table(final_data, "tidydata.txt", sep="\t")
+
